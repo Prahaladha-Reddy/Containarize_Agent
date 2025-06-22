fdd67be01705c01d07b1221debbae4a13b021390
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2025-06-22 12:54:02 +0530

    Added the env

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+import os
 app = FastAPI()
-
+MY_PROJECT_NAME=os.getenv("MY_PROJECT_NAME")
+MY_APP=os.getenv("MY_APP")
 @app.get("/")
 def read_root():
-    return {"Hello": "World again"}
+    return {"Hello": "World again", "MY_PROJECT_NAME": MY_PROJECT_NAME, "MY_APP": MY_APP}
